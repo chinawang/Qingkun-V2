a00d8b14e7b062cf5106fdffcbec56414dac10b7
Wangyx <chinawangyx@hotmail.com>
2019-01-13 12:25:28 +0800

    update database

--- qingkun/vendor/composer/autoload_static.php
+++ qingkun/vendor/composer/autoload_static.php
@@ -356,6 +356,7 @@ class ComposerStaticInit669c95193cab03cd306c11c2feb1a31f
         'CreateAwardsTable' => __DIR__ . '/../..' . '/database/migrations/2018_08_17_080720_create_awards_table.php',
         'CreateBannersTable' => __DIR__ . '/../..' . '/database/migrations/2018_08_11_152704_create_banners_table.php',
         'CreateEmployeesTable' => __DIR__ . '/../..' . '/database/migrations/2017_09_17_085330_create_employees_table.php',
+        'CreateExpertisesTable' => __DIR__ . '/../..' . '/database/migrations/2019_01_13_042141_create_expertises_table.php',
         'CreateJobsTable' => __DIR__ . '/../..' . '/database/migrations/2018_08_11_152643_create_jobs_table.php',
         'CreateLogsTable' => __DIR__ . '/../..' . '/database/migrations/2017_06_04_042933_create_logs_table.php',
         'CreateNewsTable' => __DIR__ . '/../..' . '/database/migrations/2018_08_11_153447_create_news_table.php',
