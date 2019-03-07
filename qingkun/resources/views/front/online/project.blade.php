ea315a2894afb1ed652a5b7714b4b1fbbbd11cfd
Wangyx <chinawangyx@hotmail.com>
2019-03-07 09:15:21 +0800

    update

--- qingkun/resources/views/front/online/project.blade.php
+++ qingkun/resources/views/front/online/project.blade.php
@@ -84,7 +84,7 @@
                     <li class="view-content">
                         <a href="/project/detail/{{ $project->id }}" class="view-item">
                             <img class="view-img" src="{{ $project->photo_large_1 }}">
-                            <div class="overlay"></div>
+                            {{--<div class="overlay"></div>--}}
                             <span class="view-subtext">{{ $project->provence_name }}</span>
                             <span class="view-text">{{ $project->name }}</span>
                         </a>
